8c5404778f2b823fb135d0560074f30febb1f45c
ashtyn3 <ashtynmorel@gmail.com>
2022-03-21 16:35:42 -0400

    feat(fs): update func signatures & convert buffer to int

--- fs/fs.h
+++ fs/fs.h
@@ -6,17 +6,24 @@
 
 typedef struct block {
   int start;
-  unsigned char buffer[4000];
+  int buffer[4000];
   // 1000000000 / 10 =  100000000
   // 250000
 
-  struct block *next;
 } block;
 
-block init(unsigned int);
-block *find(block *, unsigned int, int);
+typedef struct drive {
+  char *path;
+  block dirty;
+} drive;
 
-int alloc_dir(block *, unsigned int, int, const char[]);
-int make_pointer(block *);
+drive init(unsigned int, char *img);
+block *find(drive *, unsigned int, int);
+
+int alloc_dir(drive *, unsigned int, int, const char[]);
+int make_pointer(drive *);
 int append_dir(block *root, unsigned int size, int dir_start, int item_start,
                const char name[]);
+
+void write_block(drive *, block *);
+void update_header(drive *);
