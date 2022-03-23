937a8607f8516371601d31d86ba077fab3374f9c
ashtyn3 <ashtynmorel@gmail.com>
2022-03-23 14:12:22 -0400

    refactor(fs): functions use drive struct instead of root

--- fs/fs.h
+++ fs/fs.h
@@ -5,16 +5,16 @@
 #include <stdlib.h>
 
 typedef struct block {
-  int start;
-  int buffer[4000];
-  // 1000000000 / 10 =  100000000
-  // 250000
+	int start;
+	int buffer[4000];
+	// 1000000000 / 10 =  100000000
+	// 250000
 
 } block;
 
 typedef struct drive {
-  char *path;
-  block dirty;
+	char *path;
+	block dirty;
 } drive;
 
 drive init(unsigned int, char *img);
@@ -22,8 +22,8 @@ block *find(drive *, unsigned int, int);
 
 int alloc_dir(drive *, unsigned int, int, const char[]);
 int make_pointer(drive *);
-int append_dir(block *root, unsigned int size, int dir_start, int item_start,
-               const char name[]);
+int append_dir(drive *root, unsigned int size, int dir_start, int item_start,
+	       const char name[]);
 
 void write_block(drive *, block *);
 void update_header(drive *);
