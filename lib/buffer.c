ed8122a452fd153e3813d0e3ca6aa01ed1e5edb0
ashtyn3 <ashtynmorel@gmail.com>
2022-03-23 14:15:08 -0400

    refactor(buffer): compliant with b tree pointers

--- lib/buffer.c
+++ lib/buffer.c
@@ -1,24 +1,26 @@
 #include "buffer.h"
+#include "stdlib.h"
 
-void print_buf(const int buf[]) {
-  printf("{");
-  size_t n = sizeof(*buf);
-  for (int i = 0; i < n; i++) {
-    printf(" %d ", buf[i]);
-  }
-  printf("}\n");
+void print_buf(const int buf[])
+{
+	printf("{");
+	for (int i = 0; i < 100; i++) {
+		printf(" %d ", buf[i]);
+	}
+	printf("}\n");
 }
 
-char *get_name(const char buf[]) {
-  int start_id = buf[0];
-  int name_len = buf[1];
+char *get_name(const int buf[])
+{
+	int start_id = buf[0];
+	int name_len = buf[4];
 
-  char *name = {0};
+	int *name = (int *)malloc(name_len * sizeof(int));
 
-  int offset = 0;
-  for (int i = 2; i < name_len; i++) {
-    name[offset] = buf[i];
-    offset++;
-  }
-  return name;
+	int offset = 0;
+	for (int i = 0; i < name_len; i++) {
+		name[offset] = buf[i + 5];
+		offset++;
+	}
+	return (char *)name;
 }
