46397c65d695ea3e78067ce401d7d26e4322af2e
ashtyn3 <ashtynmorel@gmail.com>
2022-03-26 06:50:17 -0400

    fix(config): uses and generates config.h

--- config/menu.py
+++ config/menu.py
@@ -10,14 +10,14 @@ import mmap
 def clear_last_line():
     sys.stdout.write("\033[F")
     sys.stdout.write("\033[K")
-choices =	{
+choices = {
   "1": {
       "title": "FS_SIZE",
       "type": "int",
       "args":{
           "prompt": "Size"
           },
-      "set": False,
+      "value": "",
       "rec": True
       },
   "2": {
@@ -26,7 +26,7 @@ choices =	{
       "args":{
             "prompt": "Image"
           },
-      "set": False,
+      "value": "",
       "rec": True
       },
 }
@@ -57,39 +57,47 @@ def remove_dup():
 
     return lines
 
+def make_macros():
+    macros = []
+    for key in choices:
+        choice = choices[key]
+        if choice["value"] != "":
+            macros.append("#define "+choice["title"] +" "+choice["value"])
+    return macros
+
 def exit_save():
     clear_last_line()
     base = findPath("WORKSPACE")
     answers = remove_dup()
-    f = open(base+"/fs/config.h", "a")
-    f.write("\n".join(answers))
+    f = open(base+"/fs/config.h", "w")
+    f.write("\n".join(make_macros()))
     f.close()
-    with open(base+"/fs/config.h") as f2:
-        s = mmap.mmap(f2.fileno(), 0, access=mmap.ACCESS_READ)
-        for key in choices:
-            if key == "0":
-                continue
-            if s.find(bytes(choices[key]["title"], "UTF-8")) == -1 and choices[key]["rec"] == True:
-                console.print(" [red bold]ERROR:[reset] "+choices[key]["title"]+" is possibly not set")
-                return
     quit()
-with console.screen():
-    console.print("[bold]Ashtfs configuration tool", justify="center")
-    console.print("Choose value to set:")
-    console.print("(0): save & exit")
-    console.print("(1): Filesystem size in KB")
-    console.print("(2): Image path")
-    while True:
-        num = Prompt.ask(choices=["0",*choices.keys()])
-        if num == "0":
-            exit_save()
-            continue
-        clear_last_line()
-        choice = choices[num]
-        value = Prompt.ask(**choice["args"])
-        clear_last_line()
-        console.print("[green bold] "+choice["title"]+"[reset] = " + value)
-        if choice["type"] == "int":
-            answers.append("#define "+choice["title"]+" "+value)
-        if choice["type"] == "str":
-            answers.append("#define "+choice["title"]+" "+"\""+value+"\"")
+
+def get_defaults():
+    for item in choices:
+        if choices[item]["value"] != "":
+            console.print("[green bold] "+choices[item]["title"]+"[reset] = " + choices[item]["value"])
+def main():
+    with console.screen():
+        console.print("[bold]Ashtfs configuration tool", justify="center")
+        console.print("Choose value to set:")
+        console.print("(0): save & exit")
+        console.print("(1): Filesystem size in KB")
+        console.print("(2): Image path")
+        console.print("================")
+        get_defaults()
+        while True:
+            num = Prompt.ask(choices=["0",*choices.keys()])
+            if num == "0":
+                exit_save()
+                continue
+            clear_last_line()
+            choice = choices[num]
+            value = Prompt.ask(**choice["args"])
+            clear_last_line()
+            console.print("[green bold] "+choice["title"]+"[reset] = " + value)
+            if choice["type"] == "int":
+                choices[num]["value"] = value;
+            if choice["type"] == "str":
+                choices[num]["value"] = "\""+value+"\""
